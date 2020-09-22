342e3688b71dc863db583c3382b75db688f0dddc
greddy992 <greddy992@gmail.com>
2020-09-21 23:55:52 -0400

    Update setup_bass.py
    Issues with Cython's include_path keyword within cythonize in newer versions. Root of the problem is still unknown.

--- setup_bass.py
+++ setup_bass.py
@@ -1,6 +1,7 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
 setup(name='BASS',
-      ext_modules=cythonize("bass.pyx",  compiler_directives={'language_level' : "3"},include_path=[np.get_include()]))
+      ext_modules=cythonize(Extension("bass",sources=["bass.pyx"],include_dirs=[np.get_include()]), compiler_directives={'language_level' : "3"})
+      )
